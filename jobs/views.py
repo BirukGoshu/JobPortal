191a513e4db37614d8e0713a7781402a2c68acec
BirukGoshu <birukgoshu@gmail.com>
2022-04-04 09:49:55 +0300

    main

--- jobs/views.py
+++ jobs/views.py
@@ -8,33 +8,33 @@ from django.contrib.auth.models import User,auth
 from .models import Job
 
 def index(request):
-    if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-        user = auth.authenticate(username=username,password=password)
-        
-        if user is not None:
-            auth.login(request, user)
-            Job.objects.create(position_name=request.POST['position_name'],
-                            text_description=request.POST['description'], 
-                            min_age=request.POST['min_age'], 
-                            max_age=request.POST['max_age'],
-                            salary=request.POST['salary'], 
-                            number_of_opening=request.POST['num_of_opening'],
-                            creater = request.user,)
-            messages.info(request, 'job posted successfully')
-        else:
-            messages.info(request,"invalid credentials")
-            return render(request,'jobs/index.html')
-    else:
-        return render(request,'jobs/index.html')
-        
     # print(request.POST)
     return render(request, "jobs/index.html")
 
 def post_job(request):
-    pass
-
+    if request.method == 'POST':
+        # username = request.POST['username']
+        # password = request.POST['password']
+        # user = auth.authenticate(username=username,password=password)
+        if request.user is authenticated:
+            if user is not None:
+                auth.login(request, user)
+                Job.objects.create(position_name=request.POST['position_name'],
+                                text_description=request.POST['description'], 
+                                min_age=request.POST['min_age'], 
+                                max_age=request.POST['max_age'],
+                                salary=request.POST['salary'], 
+                                number_of_opening=request.POST['num_of_opening'],
+                                creater = request.user,)
+                messages.info(request, 'job posted successfully')
+            else:
+                messages.info(request,"invalid credentials")
+                return render(request,'jobs/index.html')
+        else:
+            return redirect(request,'jobs.login.html')
+    else:
+        return render(request,'jobs/index.html')
+    
 def about(request):
     return render(request, 'jobs/about.html')
 
